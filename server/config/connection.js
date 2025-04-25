f6264b48c42056f0f60fbbf35000b7f5b2bf110b
mohsinansare <mohsinansare@gmail.com>
2025-04-24 23:50:42 -0400

    updated files

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,18 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
 
+mongoose.set('debug', true);
 // Call connectDB to establish the connection
 connectDB();
 
